405e5021c19f9e3f349d0ab460c1cff64fced515
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-08-22 21:07:47 -0300

    Eliminate landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home/Home.jsx";
-import Landing from "./components/Landing/Landing.jsx";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact>
-          <Landing />
-        </Route>
-        <Route path="/home" exact>
           <Home />
         </Route>
       </Switch>
